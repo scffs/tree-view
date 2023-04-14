45b0751ab8418a977bdba1b556cac1420b5a599e
scffs <67464545+scffs@users.noreply.github.com>
2023-04-14 16:56:35 +0700

    format code

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
 
 import './App.css';
-import Button from "./components/Button/Button.jsx";
-import Footer from "./components/Footer/Footer.jsx";
+
+import Button from './components/Button/Button.jsx';
+import Footer from './components/Footer/Footer.jsx';
 
 const App = () => {
-    // TODO: move logic of Tree to another component
     const [selectedNode, setSelectedNode] = useState(null);
     const [nodeCount, setNodeCount] = useState(0);
 
@@ -17,26 +17,25 @@ const App = () => {
                 id: 2,
                 name: 'Node 2',
                 children: [
-                    {id: 3, name: 'Node 3', children: []},
-                    {id: 4, name: 'Node 4', children: []},
+                    { id: 3, name: 'Node 3', children: [] },
+                    { id: 4, name: 'Node 4', children: [] },
                 ],
             },
-            {id: 5, name: 'Node 5', children: []},
+            { id: 5, name: 'Node 5', children: [] },
         ]
-    }
+    };
 
     const [tree, setTree] = useState(baseTree);
 
     const addNode = () => {
         const newNode = { name: `New Node ${nodeCount}`, children: [] };
+
         if (!selectedNode) {
-            // If no node is selected, add the new node as a child of the root
             setTree((prevState) => ({
                 ...prevState,
                 children: [...prevState.children, newNode],
             }));
         } else {
-            // If a node is selected, add the new node as its child
             setTree((prevState) => ({
                 ...prevState,
                 children: updateChild(prevState.children, selectedNode, (child) => ({
@@ -55,13 +54,12 @@ const App = () => {
     };
 
     const removeNode = () => {
-        if (!selectedNode) {
-            return null;
-        }
+        if (!selectedNode) return null;
+
         setTree((prevState) => {
-            if (selectedNode.id === prevState.id) setTree(baseTree);
-            else {
-                // If the selected node is not the root, remove it from its parent's children array
+            if (prevState.id === selectedNode.id) {
+                return baseTree;
+            } else {
                 return {
                     ...prevState,
                     children: prevState.children.map((child) =>
@@ -78,10 +76,9 @@ const App = () => {
         setSelectedNode(null);
     };
 
+
     const editNode = () => {
-        if (!selectedNode) {
-            return null;
-        }
+        if (!selectedNode) return null;
 
         const newName = prompt('Enter new node name', selectedNode.name);
 
@@ -117,12 +114,13 @@ const App = () => {
     const resetNode = () => {
         return setTree(baseTree)
     }
+
     return (
         <div className='wrapper'>
             <div className='box'>
-               <div className='box-top'>
-                   <h1 className='title'>Tree</h1>
-               </div>
+                <div className='box-top'>
+                    <h1 className='title'>Tree</h1>
+                </div>
                 <div className='box-bottom'>
                     <ul className='node-list'>
                         {renderNode(tree)}
